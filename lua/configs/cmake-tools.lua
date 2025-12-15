d2ddf5f3f2728326f832e5b45e7d3ff250f2a81d
Altamash Aslam <aaslam12@users.noreply.github.com>
2025-12-15 14:09:46 -0700

    done

--- lua/configs/cmake-tools.lua
+++ lua/configs/cmake-tools.lua
@@ -1,10 +1,16 @@
+local map = vim.keymap.set
 require("cmake-tools").setup({
-  cmake_dap_configuration = {
-    name = "Launch file",
-    type = "codelldb",
-    request = "launch",
-    stopOnEntry = false,
-    console = "internalConsole",
-    show_console = "always"
-  },
+    cmake_dap_configuration = {
+        name = "Launch file",
+        type = "codelldb",
+        request = "launch",
+        stopOnEntry = false,
+        console = "internalConsole",
+        show_console = "always",
+    },
 })
+
+map("n", "<leader>dad", "<cmd>CMakeDebug<cr>", { desc = "CMake: Debug" })
+map("n", "<leader>dav", "<cmd>CMakeDebug<cr>", { desc = "CMake: Debug" })
+map("n", "<leader>dal", "<cmd>CMakeSelectLaunchTarget<cr>", { desc = "CMake: Select Launch Target" })
+
